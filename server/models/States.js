8815ff65c30a5e4b892e16905a2b309ba25fc180
thefourthlion <the00110100lion@gmail.com>
2025-04-17 07:05:02 -0700

    format server

--- server/models/States.js
+++ server/models/States.js
@@ -1,1 +1,54 @@
-const mongoose = require("mongoose"); const StatesSchema = new mongoose.Schema( {MedianHomePrice: { type: String, required: [true, "Please provide MedianHomePrice"],  },CapitalGainsTax: { type: String, required: [true, "Please provide CapitalGainsTax"],  },IncomeTax: { type: String, required: [true, "Please provide IncomeTax"],  },SalesTax: { type: String, required: [true, "Please provide SalesTax"],  },PropertyTaxes: { type: String, required: [true, "Please provide PropertyTaxes"],  },Abortion: { type: String, required: [true, "Please provide Abortion"],  },CostOfLiving: { type: String, required: [true, "Please provide CostOfLiving"],  },K12SchoolPerformance: { type: String, required: [true, "Please provide K12SchoolPerformance"],  },HigherEdSchoolPerformance: { type: String, required: [true, "Please provide HigherEdSchoolPerformance"],  },ForestedLand: { type: String, required: [true, "Please provide ForestedLand"],  },GunLaws: { type: String, required: [true, "Please provide GunLaws"],  },MinimumWage: { type: String, required: [true, "Please provide MinimumWage"],  },Population: { type: String, required: [true, "Please provide Population"],  },ViolentCrimes: { type: String, required: [true, "Please provide ViolentCrimes"],  },PoliticalLeaning: { type: String, required: [true, "Please provide PoliticalLeaning"],  },Name: { type: String, required: [true, "Please provide Name"],  }, }, { timestamps: true } ); const States = mongoose.model("States", StatesSchema); module.exports = States;
+const mongoose = require("mongoose");
+const StatesSchema = new mongoose.Schema(
+  {
+    MedianHomePrice: {
+      type: String,
+      required: [true, "Please provide MedianHomePrice"],
+    },
+    CapitalGainsTax: {
+      type: String,
+      required: [true, "Please provide CapitalGainsTax"],
+    },
+    IncomeTax: { type: String, required: [true, "Please provide IncomeTax"] },
+    SalesTax: { type: String, required: [true, "Please provide SalesTax"] },
+    PropertyTaxes: {
+      type: String,
+      required: [true, "Please provide PropertyTaxes"],
+    },
+    Abortion: { type: String, required: [true, "Please provide Abortion"] },
+    CostOfLiving: {
+      type: String,
+      required: [true, "Please provide CostOfLiving"],
+    },
+    K12SchoolPerformance: {
+      type: String,
+      required: [true, "Please provide K12SchoolPerformance"],
+    },
+    HigherEdSchoolPerformance: {
+      type: String,
+      required: [true, "Please provide HigherEdSchoolPerformance"],
+    },
+    ForestedLand: {
+      type: String,
+      required: [true, "Please provide ForestedLand"],
+    },
+    GunLaws: { type: String, required: [true, "Please provide GunLaws"] },
+    MinimumWage: {
+      type: String,
+      required: [true, "Please provide MinimumWage"],
+    },
+    Population: { type: String, required: [true, "Please provide Population"] },
+    ViolentCrimes: {
+      type: String,
+      required: [true, "Please provide ViolentCrimes"],
+    },
+    PoliticalLeaning: {
+      type: String,
+      required: [true, "Please provide PoliticalLeaning"],
+    },
+    Name: { unique: true, type: String, required: [true, "Please provide Name"] },
+  },
+  { timestamps: true }
+);
+const States = mongoose.model("States", StatesSchema);
+module.exports = States;
--- server/routes/States.js
+++ server/routes/States.js
@@ -1,1 +1,15 @@
-const express = require("express"); const router = express.Router(); const { createStates, readStates, readStatesFromID, updateStates, deleteStates, } = require("../controllers/States"); router.route("/create").post(createStates); router.route("/read").get(readStates); router.route("/read/:id").get(readStatesFromID); router.route("/update/:id").post(updateStates); router.route("/delete/:id").delete(deleteStates); module.exports = router; 
+const express = require("express");
+const router = express.Router();
+const {
+  createStates,
+  readStates,
+  readStatesFromID,
+  updateStates,
+  deleteStates,
+} = require("../controllers/States");
+router.route("/create").post(createStates);
+router.route("/read").get(readStates);
+router.route("/read/:id").get(readStatesFromID);
+router.route("/update/:id").post(updateStates);
+router.route("/delete/:id").delete(deleteStates);
+module.exports = router;
